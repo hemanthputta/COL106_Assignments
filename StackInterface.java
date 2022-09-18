13d3f64fc21c3be16c8e8f3362dff61d8580280f
hemanthputta <113828941+hemanthputta@users.noreply.github.com>
2022-09-18 16:25:25 +0530

    Delete StackInterface.java

--- StackInterface.java
+++
@@ -1,9 +0,0 @@
-import java.util.EmptyStackException;
-
-public interface StackInterface{
-	public void push(Object o);
-	public Object pop() throws Exception;
-	public Object top() throws Exception;
-	public boolean isEmpty();
-	public String toString();
-}
